fc1872c32082c0d44f9ce30c5434a56369dcdd50
n-jabes <nshutij7@gmail.com>
2024-05-02 16:03:28 +0200

    finished the bottom section of the single page

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,8 @@
+import './slider.scss'
+function Slider(){
+  return (
+    <div className='slider'>Slider</div>
+  )
+}
+
+export default Slider;
--- src/routes/singlePage/SinglePage.jsx
+++ src/routes/singlePage/SinglePage.jsx
@@ -1,8 +1,39 @@
-import './singlePage.scss'
-function SinglePage(){
+import Slider from '../../components/slider/Slider';
+import './singlePage.scss';
+import { singlePostData, userData } from '../../lib/dummydata';
+function SinglePage() {
   return (
-    <div className='singlePage'>SinglePage</div>
-  )
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1 className="title">{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">
+              {singlePostData.description}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
 }
 
 export default SinglePage;
