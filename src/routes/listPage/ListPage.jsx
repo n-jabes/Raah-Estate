30a0406753744f8788225006bfe006f14e99da38
n-jabes <nshutij7@gmail.com>
2024-05-05 14:25:38 +0200

    added some responsiveness

--- src/routes/listPage/ListPage.jsx
+++ src/routes/listPage/ListPage.jsx
@@ -10,9 +10,11 @@ function ListPage() {
       <div className="listContainer">
         <div className="wrapper">
           <Filter />
+          <div className='listCollection'>
           {data.map(item=>(
             <Card key={item.id} item={item}/>
           ))}
+          </div>
         </div>
       </div>
       <div className="mapContainer">
